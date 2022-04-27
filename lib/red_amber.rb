f3fd8a95ca5abb50406bfc0a98468fc726bb266b
Hirokazu SUZUKI (heronshoes) <63298319+heronshoes@users.noreply.github.com>
2022-04-27 09:51:41 +0900

    :butterfly: Fix require to require_relative

--- lib/red_amber.rb
+++ lib/red_amber.rb
@@ -2,12 +2,12 @@
 
 require 'arrow'
 
-require 'red_amber/data_frame_output'
-require 'red_amber/data_frame_selectable'
-require 'red_amber/data_frame'
-require 'red_amber/vector_functions'
-require 'red_amber/vector'
-require 'red_amber/version'
+require_relative 'red_amber/data_frame_output'
+require_relative 'red_amber/data_frame_selectable'
+require_relative 'red_amber/data_frame'
+require_relative 'red_amber/vector_functions'
+require_relative 'red_amber/vector'
+require_relative 'red_amber/version'
 
 module RedAmber
   class Error < StandardError; end
