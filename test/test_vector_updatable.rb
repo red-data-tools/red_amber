8880b396409fd663339b2768612ef20686490b80
Hirokazu SUZUKI (heronshoes) <heronshoes877@gmail.com>
2022-06-19 16:18:06 +0900

    :white_check_mark: Move if_else test  to updatable

--- test/test_vector_updatable.rb
+++ test/test_vector_updatable.rb
@@ -54,6 +54,53 @@ class VectorTest < Test::Unit::TestCase
     end
   end
 
+  sub_test_case('Ternary function #if_else') do
+    setup do
+      @empty = Vector.new([])
+      @boolean = Vector.new([true, true, false, nil])
+      @integer = Vector.new([1, 2, 3, nil])
+      @double = Vector.new([1.0, Float::NAN, Float::INFINITY, nil])
+      @string = Vector.new(%w[A B C] << nil)
+    end
+
+    test 'not a boolean' do
+      assert_raise(RedAmber::VectorTypeError) { @integer.if_else(@integer, @integer) }
+    end
+
+    test 'empty' do
+      assert_raise(Arrow::Error::NotImplemented) { @boolean.if_else(@empty, @integer) }
+      assert_raise(Arrow::Error::NotImplemented) { @boolean.if_else(@integer, @empty) }
+    end
+
+    test 'scalar or scalar' do
+      assert_equal [1, 1, 0, nil], @boolean.if_else(1, 0).to_a
+      assert_raise(ArgumentError) { @boolean.if_else(1, nil) }
+      assert_raise(Arrow::Error::NotImplemented) { @boolean.if_else(1, 'A') }
+    end
+
+    test 'array or scalar' do
+      assert_equal [1, 2, 0, nil], @boolean.if_else([1, 2, 3, 4], 0).to_a
+    end
+
+    test 'array or array' do
+      assert_equal [1, 2, 0, nil], @boolean.if_else([1, 2, 3, 4], [0, 0, 0, nil]).to_a
+      assert_raise(Arrow::Error::NotImplemented) { @boolean.if_else([1, 2, 3, 4], [nil, nil, nil, nil]) }
+      assert_equal [1, 2, nil, nil], @boolean.if_else([1, 2, 3, 4], Arrow::UInt8Array.new([nil, nil, nil, nil])).to_a
+    end
+
+    test 'vector or scalar' do
+      assert_equal [1, 2, 0, nil], @boolean.if_else(@integer, 0).to_a
+    end
+
+    test 'vector(integer) or vector(double)' do
+      assert_equal [1.0, 2.0, Float::INFINITY, nil], @boolean.if_else(@integer, @double).to_a
+    end
+
+    test 'vector(integer) or vector(string)' do
+      assert_raise(Arrow::Error::NotImplemented) { @boolean.if_else(@integer, @string) }
+    end
+  end
+
   sub_test_case '#primitive_invert' do
     test '#primitive_invert' do
       assert_raise(VectorTypeError) { Vector.new([1]).primitive_invert }
