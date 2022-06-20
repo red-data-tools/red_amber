6872b8b801d27b95fd00ea823a50e5cc5ab297b9
Hirokazu SUZUKI (heronshoes) <heronshoes877@gmail.com>
2022-06-20 17:53:30 +0900

    :recycle: Delete unused methods

--- lib/red_amber/data_frame_helper.rb
+++ lib/red_amber/data_frame_helper.rb
@@ -5,27 +5,6 @@ module RedAmber
   module DataFrameHelper
     private
 
-    def expand_range(args)
-      args.each_with_object([]) do |e, a|
-        e.is_a?(Range) ? a.concat(normalized_array(e)) : a.append(e)
-      end
-    end
-
-    def normalized_array(range)
-      both_end = [range.begin, range.end]
-      both_end[1] -= 1 if range.exclude_end? && range.end.is_a?(Integer)
-
-      if both_end.any?(Integer) || both_end.all?(&:nil?)
-        if both_end.any? { |e| e&.>=(size) || e&.<(-size) }
-          raise DataFrameArgumentError, "Index out of range: #{range} for 0..#{size - 1}"
-        end
-
-        (0...size).to_a[range]
-      else
-        range.to_a
-      end
-    end
-
     def out_of_range?(indeces)
       indeces.max >= size || indeces.min < -size
     end
@@ -44,10 +23,6 @@ module RedAmber
 
     def create_dataframe_from_vector(key, vector)
       DataFrame.new(key => vector.data)
-    end
-
-    def select_obs_by_boolean(array)
-      DataFrame.new(@table.filter(array))
     end
 
     def keys_by_booleans(booleans)
