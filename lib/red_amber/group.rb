b022b1f0f7e79dc05a8660e98895a8ae09c0501b
Hirokazu SUZUKI (heronshoes) <heronshoes877@gmail.com>
2022-10-14 18:28:10 +0900

    :recycle: Refactor Group to align with Arrow

--- lib/red_amber/group.rb
+++ lib/red_amber/group.rb
@@ -12,7 +12,7 @@ module RedAmber
       @table = @dataframe.table
       @group_keys = group_keys.flatten
 
-      raise GroupArgumentError, 'group_keys is empty.' if @group_keys.empty?
+      raise GroupArgumentError, 'group_keys are empty.' if @group_keys.empty?
 
       d = @group_keys - @dataframe.keys
       raise GroupArgumentError, "#{d} is not a key of\n #{@dataframe}." unless d.empty?
@@ -23,7 +23,26 @@ module RedAmber
     functions = %i[count sum product mean min max stddev variance]
     functions.each do |function|
       define_method(function) do |*summary_keys|
-        by(function, summary_keys)
+        summary_keys = Array(summary_keys).flatten
+        d = summary_keys - @dataframe.keys
+        raise GroupArgumentError, "#{d} is not a key of\n #{@dataframe}." unless summary_keys.empty? || d.empty?
+
+        table = @group.aggregate(*build_aggregation_keys("hash_#{function}", summary_keys))
+        df = DataFrame.new(table)
+        df.pick(@group_keys, df.keys - @group_keys)
+      end
+    end
+
+    alias_method :__count, :count
+    private :__count
+
+    def count(*summary_keys)
+      df = __count(summary_keys)
+      # if counts are the same (and do not include NaN or nil), aggregate count columns.
+      if df.pick(@group_keys.size..).to_h.values.uniq.size == 1
+        df.pick(0..@group_keys.size).rename { [keys[-1], :count] }
+      else
+        df
       end
     end
 
@@ -48,18 +67,12 @@ module RedAmber
 
     private
 
-    def by(func, summary_keys)
-      summary_keys = Array(summary_keys).flatten
-      d = summary_keys - @dataframe.keys
-      raise GroupArgumentError, "#{d} is not a key of\n #{@dataframe}." unless summary_keys.empty? || d.empty?
-
-      df = RedAmber::DataFrame.new(@group.send(func, *summary_keys))
-      df = df.pick(@group_keys, df.keys - @group_keys)
-      # if counts are the same (and do not include NaN or nil), aggregate count columns.
-      if func == :count && df.pick(@group_keys.size..).to_h.values.uniq.size == 1
-        df = df.pick(0..@group_keys.size).rename { [keys[-1], :count] }
+    def build_aggregation_keys(function_name, summary_keys)
+      if summary_keys.empty?
+        [function_name]
+      else
+        summary_keys.map { |key| "#{function_name}(#{key})" }
       end
-      df
     end
   end
 end
