cf9bb3250235f321ccf98951ae0d38c963126bcc
Hirokazu SUZUKI (heronshoes) <heronshoes877@gmail.com>
2022-10-19 18:16:25 +0900

    :butterfly: Fix DF#join to merge key columns

--- lib/red_amber/data_frame_combinable.rb
+++ lib/red_amber/data_frame_combinable.rb
@@ -77,5 +77,47 @@ module RedAmber
     end
 
     alias_method :bind_cols, :merge
+
+    # Undocumented
+
+    # Join other dataframe
+    #
+    # @param right [DataFrame, Arrow::Table] DataFrame/Table to be joined with self.
+    # @param join_keys [String, Symbol, ::Array<String, Symbol>] Keys to match.
+    # @return [DataFrame] Joined dataframe.
+    #
+    #   :type is one of %i[left_semi right_semi left_anti right_anti inner left_outer right_outer full_outer]
+    def join(right, join_keys, type: :inner, left_outputs: nil, right_outputs: nil)
+      right = right.table if right.is_a?(DataFrame)
+
+      join_keys = Array(join_keys).map(&:to_sym)
+
+      # Red Arrow's #join returns duplicated join_keys from self and right as of v9.0.0 .
+      # Temprally merge key vectors here to workaround.
+      table_output =
+        table.join(right, join_keys, type: type, left_outputs: left_outputs, right_outputs: right_outputs)
+      selected_indexes =
+        [*0...n_keys, *((n_keys + join_keys.size)...table_output.n_columns)]
+      merged_columns = join_keys.map.with_index do |_key, i|
+        merge_column(table_output[i], table_output[n_keys + i], type)
+      end
+
+      DataFrame.new(table_output[selected_indexes])
+               .assign(*join_keys) { merged_columns }
+    end
+
+    private
+
+    def merge_column(column1, column2, type)
+      a1 = column1.to_a
+      a2 = column2.to_a
+      if type == :full_outer
+        a1.zip(a2).map { |x, y| x || y }
+      elsif type.start_with?('right')
+        a2
+      else # :inner or :left-*
+        a1
+      end
+    end
   end
 end
