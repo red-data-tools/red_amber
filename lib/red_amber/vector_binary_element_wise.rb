4629278681d8563b1f9d014c0392ee45d2c606aa
Hirokazu SUZUKI <heronshoes877@gmail.com>
2023-02-05 06:28:31 +0900

    :memo: Update document for dynamically generated methods

--- lib/red_amber/vector_binary_element_wise.rb
+++ lib/red_amber/vector_binary_element_wise.rb
@@ -9,6 +9,11 @@ module RedAmber
     class << self
       private
 
+      # @!macro [attach] define_binary_element_wise
+      #   @!method $1(other)
+      #   [Binary element-wise function] Returns a Vector.
+      #   @param other [Vector] other operand.
+      #
       def define_binary_element_wise(function)
         define_method(function) do |other, **options|
           datum = exec_func_binary(function, other, options)
@@ -16,6 +21,11 @@ module RedAmber
         end
       end
 
+      # @!macro [attach] define_binary_element_wise_logical
+      #   @!method $1(other)
+      #   [Binary element-wise function] Returns a Vector.
+      #   @param other [Vector] other operand.
+      #
       def define_binary_element_wise_logical(method, function)
         define_method(method) do |other, **options|
           datum = exec_func_binary(function, other, options)
@@ -23,6 +33,12 @@ module RedAmber
         end
       end
 
+      # @!macro [attach] define_binary_element_wise_operator
+      #   @!method $1(other)
+      #   [Binary element-wise function] Returns a Vector.
+      #
+      #   @!method $2(other)
+      #
       def define_binary_element_wise_operator(function, operator)
         define_method(function) do |other, **options|
           datum = exec_func_binary(function, other, options)
@@ -36,69 +52,105 @@ module RedAmber
       end
     end
 
-    # [Binary element-wise]: vector.func(other) => vector
-
+    # @return [Vector] atan2 of self and other.
     define_binary_element_wise(:atan2)
 
+    # @return [Vector] and not of self and other.
     define_binary_element_wise(:and_not)
 
+    # @return [Vector] and not kleene of self and other.
     define_binary_element_wise(:and_not_kleene)
 
+    # @return [Vector] bit wise and of self and other.
     define_binary_element_wise(:bit_wise_and)
 
+    # @return [Vector] bit wise or of self and other.
     define_binary_element_wise(:bit_wise_or)
 
+    # @return [Vector] bit wise xor of self and other.
     define_binary_element_wise(:bit_wise_xor)
 
+    # @return [Vector] logb of self and other.
     define_binary_element_wise(:logb)
 
-    # [Logical binary element-wise]: vector.func(other) => vector
-
+    # @return [Vector] & of self and other.
     define_binary_element_wise_logical(:'&', :and_kleene) # rubocop:disable Lint/SymbolConversion)
 
+    # @return [Vector] & of self and other.
     define_binary_element_wise_logical(:and_kleene, :and_kleene)
 
+    # @return [Vector] evacuated `and`` of self and other.
     define_binary_element_wise_logical(:and_org, :and)
 
+    # @return [Vector] | of self and other.
     define_binary_element_wise_logical(:'|', :or_kleene) # rubocop:disable Lint/SymbolConversion)
 
+    # @return [Vector] | of self and other.
     define_binary_element_wise_logical(:or_kleene, :or_kleene)
 
+    # @return [Vector] evacuated `or` of self and other.
     define_binary_element_wise_logical(:or_org, :or)
 
-    # [Binary element-wise with operator]: vector.func(other) => vector
-
+    # @param other [Vector, Numeric] other operand.
+    # @return [Vector] adddition of self and other.
     define_binary_element_wise_operator(:add, '+')
 
+    # @param other [Vector, Numeric] other operand.
+    # @return [Vector] division of self and other.
     define_binary_element_wise_operator(:divide, '/')
 
+    # @param other [Vector, Numeric] other operand.
+    # @return [Vector] multiplication of self and other.
     define_binary_element_wise_operator(:multiply, '*')
 
+    # @param other [Vector, Numeric] other operand.
+    # @return [Vector] power operation of self and other.
     define_binary_element_wise_operator(:power, '**')
 
+    # @param other [Vector, Numeric] other operand.
+    # @return [Vector] subtraction of self and other.
     define_binary_element_wise_operator(:subtract, '-')
 
+    # @param other [Vector] other operand.
+    # @return [Vector] xor of self and other.
     define_binary_element_wise_operator(:xor, '^')
 
+    # @param other [integer] amount of shift.
+    # @return [Vector] shift left of self by other.
     define_binary_element_wise_operator(:shift_left, '<<')
 
+    # @param other [integer] amount of shift.
+    # @return [Vector] shift right of self by other.
     define_binary_element_wise_operator(:shift_right, '>>')
 
+    # @param other [Vector] other operand.
+    # @return [Vector] eq of self and other by a boolean Vector.
+    define_binary_element_wise_operator(:xor, '^')
     define_binary_element_wise_operator(:equal, '==')
     alias_method :eq, :equal
 
+    # @param other [Vector] other operand.
+    # @return [Vector] gt of self and other by a boolean Vector.
     define_binary_element_wise_operator(:greater, '>')
     alias_method :gt, :greater
 
+    # @param other [Vector] other operand.
+    # @return [Vector] ge of self and other by a boolean Vector.
     define_binary_element_wise_operator(:greater_equal, '>=')
     alias_method :ge, :greater_equal
 
+    # @param other [Vector] other operand.
+    # @return [Vector] lt of self and other by a boolean Vector.
     define_binary_element_wise_operator(:less, '<')
     alias_method :lt, :less
 
+    # @param other [Vector] other operand.
+    # @return [Vector] le of self and other by a boolean Vector.
     define_binary_element_wise_operator(:less_equal, '<=')
     alias_method :le, :less_equal
 
+    # @param other [Vector] other operand.
+    # @return [Vector] ne of self and other by a boolean Vector.
     define_binary_element_wise_operator(:not_equal, '!=')
     alias_method :ne, :not_equal
   end
--- lib/red_amber/vector_unary_element_wise.rb
+++ lib/red_amber/vector_unary_element_wise.rb
@@ -9,6 +9,10 @@ module RedAmber
     class << self
       private
 
+      # @!macro [attach] define_unary_element_wise
+      #   @!method $1
+      #   [Unary element-wise function] Returns a Vector.
+      #
       def define_unary_element_wise(function)
         define_method(function) do |**options|
           datum = exec_func_unary(function, options)
@@ -16,6 +20,12 @@ module RedAmber
         end
       end
 
+      # @!macro [attach] define_unary_element_wise_operator
+      #   @!method $1
+      #   [Unary element-wise function] Returns a Vector.
+      #
+      #   @!method $2
+      #
       def define_unary_element_wise_operator(function, operator)
         define_method(function) do |**options|
           datum = exec_func_unary(function, options)
@@ -29,79 +39,104 @@ module RedAmber
       end
     end
 
-    # [Unary element-wise]: vector.func => vector
-
+    # @return [Vector] abs of each element of self.
     define_unary_element_wise(:abs)
 
+    # @return [Vector] acos of each element of self.
     define_unary_element_wise(:acos)
 
+    # @return [Vector] asin of each element of self.
     define_unary_element_wise(:asin)
 
+    # @return [Vector] sort indices of self.
     define_unary_element_wise(:array_sort_indices)
     alias_method :sort_indexes, :array_sort_indices
     alias_method :sort_indices, :array_sort_indices
     alias_method :sort_index, :array_sort_indices
 
+    # @return [Vector] atan of each element of self.
     define_unary_element_wise(:atan)
 
+    # @return [Vector] bit wise not of each element of self.
     define_unary_element_wise(:bit_wise_not)
 
+    # @return [Vector] ceil of each element of self.
     define_unary_element_wise(:ceil)
 
+    # @return [Vector] cos of each element of self.
     define_unary_element_wise(:cos)
 
+    # @return [Vector] a Vector which filled nil foward..
     define_unary_element_wise(:fill_null_backward)
     alias_method :fill_nil_backward, :fill_null_backward
 
+    # @return [Vector] a Vector which filled nil foward.
     define_unary_element_wise(:fill_null_forward)
     alias_method :fill_nil_forward, :fill_null_forward
 
+    # @return [Vector] floor of each element of self.
     define_unary_element_wise(:floor)
 
+    # @return [Vector] boolean Vector wheather each element is finite.
     define_unary_element_wise(:is_finite)
 
+    # @return [Vector] boolean Vector wheather each element is inf.
     define_unary_element_wise(:is_inf)
 
+    # @return [Vector] boolean Vector wheather each element is na.
     def is_na # rubocop:disable Naming/PredicateName
       numeric? ? (is_nil | is_nan) : is_nil
     end
 
+    # @return [Vector] boolean Vector wheather each element is nan.
     define_unary_element_wise(:is_nan)
 
+    # @return [Vector] boolean Vector wheather each element is null.
     define_unary_element_wise(:is_null)
     alias_method :is_nil, :is_null
 
+    # @return [Vector] boolean Vector wheather each element is valid.
     define_unary_element_wise(:is_valid)
 
+    # @return [Vector] ln of each element of self.
     define_unary_element_wise(:ln)
 
+    # @return [Vector] log10 of each element of self.
     define_unary_element_wise(:log10)
 
+    # @return [Vector] log1p of each element of self.
     define_unary_element_wise(:log1p)
 
+    # @return [Vector] log2 of each element of self.
     define_unary_element_wise(:log2)
 
+    # @return [Vector] round of each element of self.
     define_unary_element_wise(:round)
 
+    # @return [Vector] round to multiple of each element of self.
     define_unary_element_wise(:round_to_multiple)
 
+    # @return [Vector] sign of each element of self.
     define_unary_element_wise(:sign)
 
+    # @return [Vector] sin of each element of self.
     define_unary_element_wise(:sin)
 
+    # @return [Vector] tan of each element of self.
     define_unary_element_wise(:tan)
 
+    # @return [Vector] trunc of each element of self.
     define_unary_element_wise(:trunc)
 
+    # @return [Vector] uniq element of self.
     define_unary_element_wise(:unique)
-
     alias_method :uniq, :unique
 
-    # [Unary element-wise with operator]: vector.func => vector, op vector
-
+    # @return [Vector] not of each element of self.
     define_unary_element_wise_operator(:invert, '!')
     alias_method :not, :invert
 
+    # @return [Vector] negate of each element of self.
     define_unary_element_wise_operator(:negate, '-@')
   end
 end
