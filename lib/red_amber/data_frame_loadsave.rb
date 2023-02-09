69f864ea7316118f921b9b18cd7b01495a38a236
Hirokazu SUZUKI <heronshoes877@gmail.com>
2023-02-10 08:33:58 +0900

    :memo: Update document of DataFrameLoadSave

--- lib/red_amber/data_frame_loadsave.rb
+++ lib/red_amber/data_frame_loadsave.rb
@@ -10,30 +10,98 @@ module RedAmber
 
     # Enable `self.load` as class method of DataFrame
     module ClassMethods
-      # Load DataFrame via Arrow::Table.load
-      def load(path, options = {})
-        DataFrame.new(Arrow::Table.load(path, options))
+      # Load DataFrame via Arrow::Table.load.
+      #
+      # Format is automatically detected by extension.
+      # @!method load(input, format: nil, compression: nil, schema: nil, skip_lines: nil)
+      # @param input [path]
+      #   source path.
+      # @param format [:arrow_file, :batch, :arrows, :arrow_stream, :stream, :csv, :tsv]
+      #   format specifier.
+      # @param compression [:gzip, nil]
+      #   compression type.
+      # @param schema [Arrow::Schema]
+      #   schema of table.
+      # @param skip_lines [Regexp]
+      #   pattern of rows to skip.
+      # @return [DataFrame]
+      #   loaded DataFrame.
+      # @example Load a tsv file
+      #   DataFrame.load("file.tsv")
+      #
+      # @example Load a csv.gz file
+      #   DataFrame.load("file.csv.gz")
+      #
+      # @example Load from URI
+      #   DataFrame.load(URI("https://some_uri/file.csv"))
+      #
+      # @example Load from Buffer
+      #   DataFrame.load(Arrow::Buffer.new(<<~BUFFER), format: :csv)
+      #     name,age
+      #     Yasuko,68
+      #     Rui,49
+      #     Hinata,28
+      #   BUFFER
+      #
+      # @example Load from Buffer skipping comment line
+      #   DataFrame.load(Arrow::Buffer.new(<<~BUFFER), format: :csv, skip_lines: /^#/)
+      #     # comment
+      #     name,age
+      #     Yasuko,68
+      #     Rui,49
+      #     Hinata,28
+      #   BUFFER
+      #
+      def load(input, **options)
+        DataFrame.new(Arrow::Table.load(input, options))
       end
     end
 
     # Save DataFrame
     #
-    # @return [DataFrame] self.
-    def save(output, options = {})
+    # Format is automatically detected by extension.
+    # @!method save(output, format: nil, compression: nil, schema: nil, skip_lines: nil)
+    # @param output [path]
+    #   output path.
+    # @param format [:arrow_file, :batch, :arrows, :arrow_stream, :stream, :csv, :tsv]
+    #   format specifier.
+    # @param compression [:gzip, nil]
+    #   compression type.
+    # @param schema [Arrow::Schema]
+    #   schema of table.
+    # @param skip_lines [Regexp]
+    #   pattern of rows to skip.
+    # @return [DataFrame]
+    #   self.
+    # @example Save a csv file
+    #   DataFrame.save("file.csv")
+    #
+    # @example Save a csv.gz file
+    #   DataFrame.save("file.csv.gz")
+    #
+    # @example Save an arrow file
+    #   DataFrame.save("file.arrow")
+    #
+    def save(output, **options)
       @table.save(output, options)
       self
     end
 
     # Save and reload to cast automatically
-    #   Via tsv format file temporally as default
+    # via tsv format file temporally as default.
+    #
+    # @param format [Symbol]
+    #   format specifier.
+    # @return [DataFrame]
+    #   reloaded DataFrame.
     #
     # @note experimental feature
     def auto_cast(format: :tsv)
       return self if empty?
 
-      tempfile = Arrow::ResizableBuffer.new(1024)
-      save(tempfile, format: format)
-      DataFrame.load(tempfile, format: format)
+      buffer = Arrow::ResizableBuffer.new(1024)
+      save(buffer, format: format)
+      DataFrame.load(buffer, format: format)
     end
   end
 end
