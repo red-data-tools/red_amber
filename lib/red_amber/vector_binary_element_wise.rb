253df0789d41056d7bf38753560aa98e2d271d08
Hirokazu SUZUKI <heronshoes877@gmail.com>
2023-02-11 16:11:25 +0900

    :memo: Update document of Vector_biary_element_wise

--- lib/red_amber/vector_binary_element_wise.rb
+++ lib/red_amber/vector_binary_element_wise.rb
@@ -29,9 +29,7 @@ module RedAmber
       private
 
       # @!macro [attach] define_binary_element_wise
-      #   @!method $1(other)
       #   [Binary element-wise function] Returns a Vector.
-      #   @param other [Vector] other operand.
       #
       def define_binary_element_wise(function)
         define_method(function) do |other, **options|
@@ -41,9 +39,7 @@ module RedAmber
       end
 
       # @!macro [attach] define_binary_element_wise_logical
-      #   @!method $1(other)
       #   [Binary element-wise function] Returns a Vector.
-      #   @param other [Vector] other operand.
       #
       def define_binary_element_wise_logical(method, function)
         define_method(method) do |other, **options|
@@ -51,123 +47,341 @@ module RedAmber
           Vector.create(datum.value)
         end
       end
-
-      # @!macro [attach] define_binary_element_wise_operator
-      #   @!method $1(other)
-      #   [Binary element-wise function] Returns a Vector.
-      #
-      #   @!method $2(other)
-      #
-      def define_binary_element_wise_operator(function, operator)
-        define_method(function) do |other, **options|
-          datum = exec_func_binary(function, other, options)
-          Vector.create(datum.value)
-        end
-
-        define_method(operator) do |other, **options|
-          datum = exec_func_binary(function, other, options)
-          Vector.create(datum.value)
-        end
-      end
     end
 
-    # @return [Vector] and not of self and other.
-    define_binary_element_wise(:and_not)
+    # @!macro kleene_logic_and
+    #   This function behaves as follows with nils:
+    #   - true and nil = nil
+    #   - nil and true = nil
+    #   - false and nil = false
+    #   - nil and false = false
+    #   - nil and nil = nil
+    #   In other words, in this context a nil value really means "unknown",
+    #   and an unknown value 'and' false is always false.
 
-    # @return [Vector] and not kleene of self and other.
-    define_binary_element_wise(:and_not_kleene)
+    # @!macro kleene_logic_and_not
+    #   This function behaves as follows with nils:
+    #   - true and not nil = nil
+    #   - nil and not false = nil
+    #   - false and not nil = false
+    #   - nil and not true = false
+    #   - nil and not nil = nil
+    #   In other words, in this context a nil value really means "unknown",
+    #   and an unknown value 'and not' true is always false,
+    #   as is false 'and not' an unknown value.
 
-    # @return [Vector] bit wise and of self and other.
-    define_binary_element_wise(:bit_wise_and)
+    # @!macro kleene_logic_or
+    #   This function behaves as follows with nils:
+    #   - true or nil = true
+    #   - nil or true = true
+    #   - false or nil = nil
+    #   - nil or false = nil
+    #   - nil or nil = nil
+    #   In other words, in this context a nil value really means "unknown",
+    #   and an unknown value 'or' true is always true.
 
-    # @return [Vector] bit wise or of self and other.
-    define_binary_element_wise(:bit_wise_or)
+    # rubocop:disable Lint/SymbolConversion)
 
-    # @return [Vector] bit wise xor of self and other.
-    define_binary_element_wise(:bit_wise_xor)
+    # Logical 'and' boolean values with Kleene logic.
+    #
+    # @macro kleene_logic_and
+    # For a different nil behavior, see function {#and_org}.
+    # @!method and_kleene(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   and_kleene of self and other.
+    define_binary_element_wise :and_kleene
+    alias_method :'&', :and_kleene
 
-    # @return [Vector] logb of self and other.
-    define_binary_element_wise(:logb)
+    # Logical 'and not' boolean values.
+    #
+    # When a nil is encountered in either input, a nil is output.
+    # For a different nil behavior, see function {#and_not_kleene}.
+    # @!method and_not(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   and not of self and other.
+    #
+    define_binary_element_wise :and_not
 
-    # @return [Vector] & of self and other.
-    define_binary_element_wise_logical(:'&', :and_kleene) # rubocop:disable Lint/SymbolConversion)
+    # Logical 'and not' boolean values with Kleene logic.
+    #
+    # @macro kleene_logic_and_not
+    # For a different nil behavior, see function {#and_not}.
+    # @!method and_not_kleene(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   and not kleene of self and other.
+    #
+    define_binary_element_wise :and_not_kleene
 
-    # @return [Vector] & of self and other.
-    define_binary_element_wise_logical(:and_kleene, :and_kleene)
-
-    # @return [Vector] evacuated `and`` of self and other.
+    # Logical 'and' boolean values.
+    #
+    # When a nil is encountered in either input, a nil is output.
+    # For a different nil behavior, see function {#and_kleene}.
+    # @!method and_org(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   evacuated `and` of self and other.
+    #
     define_binary_element_wise_logical(:and_org, :and)
 
-    # @return [Vector] | of self and other.
-    define_binary_element_wise_logical(:'|', :or_kleene) # rubocop:disable Lint/SymbolConversion)
+    # Bit-wise AND of self and other by element-wise.
+    #
+    # Nil values return nil.
+    # @!method bit_wise_and(other)
+    # @param other [Vector, array-like]
+    #   integer array-like.
+    # @return [Vector]
+    #   bit wise and of self and other.
+    #
+    define_binary_element_wise :bit_wise_and
 
-    # @return [Vector] | of self and other.
-    define_binary_element_wise_logical(:or_kleene, :or_kleene)
+    # Bit-wise OR of self and other by element-wise.
+    #
+    # Nil values return nil.
+    # @!method bit_wise_or(other)
+    # @param other [Vector, array-like]
+    #   integer array-like.
+    # @return [Vector]
+    #   bit wise or of self and other.
+    #
+    define_binary_element_wise :bit_wise_or
 
-    # @return [Vector] evacuated `or` of self and other.
+    # Bit-wise XOR of self and other by element-wise.
+    #
+    # Nil values return nil.
+    # @!method bit_wise_xor(other)
+    # @param other [Vector, array-like]
+    #   integer array-like.
+    # @return [Vector]
+    #   bit wise xor of self and other.
+    #
+    define_binary_element_wise :bit_wise_xor
+
+    # Compute base `b` logarithm of self.
+    #
+    # Non positive values return -inf or NaN. Nil values return nil.
+    # @!method logb(b)
+    # @param b [Integer]
+    #   base.
+    # @return [Vector]
+    #   logb of self and other.
+    #
+    define_binary_element_wise :logb
+
+    # Logical 'or' boolean values with Kleene logic.
+    #
+    # @macro kleene_logic_or
+    # For a different nil behavior, see function {#or_org}.
+    # @!method or_kleene(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   or_kleene of self and other.
+    #
+    define_binary_element_wise :or_kleene
+    alias_method :'|', :or_kleene
+
+    # Logical 'or' boolean values.
+    #
+    # When a nil is encountered in either input, a nil is output.
+    # For a different nil behavior, see function {#or_kleene}.
+    # @!method or_org(other)
+    # @param other [Vector, array-like]
+    #   boolean array-like.
+    # @return [Vector]
+    #   evacuated `or` of self and other.
+    #
     define_binary_element_wise_logical(:or_org, :or)
 
-    # @param other [Vector, Numeric] other operand.
-    # @return [Vector] adddition of self and other.
-    define_binary_element_wise_operator(:add, '+')
+    # Add the arguments element-wise.
+    #
+    # Results will wrap around on integer overflow.
+    # @!method add(other)
+    # @param other [Vector, Numeric]
+    #   other numeric Vector or numeric scalar.
+    # @return [Vector]
+    #   adddition of self and other.
+    #
+    define_binary_element_wise :add
+    alias_method :'+', :add
 
-    # @param other [Vector, Numeric] other operand.
-    # @return [Vector] division of self and other.
-    define_binary_element_wise_operator(:divide, '/')
+    # Divide the arguments element-wise.
+    #
+    # Integer division by zero returns an error. However, integer overflow
+    # wraps around, and floating-point division by zero returns an infinite.
+    # @!method divide(divisor)
+    # @param divisor [Vector, Numeric]
+    #   numeric vector or numeric scalar as divisor.
+    # @return [Vector]
+    #   division of self and other.
+    #
+    define_binary_element_wise :divide
+    alias_method :'/', :divide
 
-    # @param other [Vector, Numeric] other operand.
-    # @return [Vector] multiplication of self and other.
-    define_binary_element_wise_operator(:multiply, '*')
+    # Multiply the arguments element-wise.
+    #
+    # Results will wrap around on integer overflow.
+    # @!method multiply(other)
+    # @param other [Vector, Numeric]
+    #   other numeric vector or numeric scalar.
+    # @return [Vector]
+    #   multiplication of self and other.
+    #
+    define_binary_element_wise :multiply
+    alias_method :'*', :multiply
 
-    # @param other [Vector, Numeric] other operand.
-    # @return [Vector] power operation of self and other.
-    define_binary_element_wise_operator(:power, '**')
+    # Raise arguments to power element-wise.
+    #
+    # Integer to negative integer power returns an error.
+    # However, integer overflow wraps around.
+    # If either self or exponent is nil the result will be nil.
+    # @!method power(exponent)
+    # @param exponent [Vector, Numeric]
+    #   numeric vector or numeric scalar as exponent.
+    # @return [Vector]
+    #   power operation of self and other.
+    #
+    define_binary_element_wise :power
+    alias_method :'**', :power
 
-    # @param other [Vector, Numeric] other operand.
-    # @return [Vector] subtraction of self and other.
-    define_binary_element_wise_operator(:subtract, '-')
+    # Subtract the arguments element-wise.
+    #
+    # Results will wrap around on integer overflow.
+    # @!method subtract(other)
+    # @param other [Vector, Numeric]
+    #   other numeric vector or numeric scalar.
+    # @return [Vector]
+    #   subtraction of self and other.
+    #
+    define_binary_element_wise :subtract
+    alias_method :'-', :subtract
 
-    # @param other [Vector] other operand.
-    # @return [Vector] xor of self and other.
-    define_binary_element_wise_operator(:xor, '^')
+    # Left shift of self by other.
+    #
+    # The shift operates as if on the two's complement representation of the number.
+    # In other words, this is equivalent to multiplying self by 2 to the power 'amount',
+    # even if overflow occurs.
+    # self is returned if 'amount' (the amount to shift by) is negative or
+    # greater than or equal to the precision of self.
+    # @!method shift_left(amount)
+    # @param amount [integer]
+    #   the amount to shift by.
+    # @return [Vector]
+    #  shift_left of self by amount.
+    #
+    define_binary_element_wise :shift_left
+    alias_method :'<<', :shift_left
 
-    # @param other [integer] amount of shift.
-    # @return [Vector] shift left of self by other.
-    define_binary_element_wise_operator(:shift_left, '<<')
+    # Right shift of self by other.
+    #
+    # This is equivalent to dividing `x` by 2 to the power `y`.
+    # Self is returned if 'amount' (the amount to shift by) is: negative or
+    # greater than or equal to the precision of self.
+    # @!method shift_right(amount)
+    # @param amount [integer]
+    #   the amount to shift by.
+    # @return [Vector]
+    #   shift_right of self by amount.
+    #
+    define_binary_element_wise :shift_right
+    alias_method :'>>', :shift_right
 
-    # @param other [integer] amount of shift.
-    # @return [Vector] shift right of self by other.
-    define_binary_element_wise_operator(:shift_right, '>>')
+    # Logical 'xor' boolean values
+    #
+    # When a nil is encountered in either input, a nil is output.
+    # @!method xor(other)
+    # @param other [Vector]
+    #   other boolean vector or boolean scalar.
+    # @return [Vector]
+    #   eq of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :xor
+    alias_method :'^', :xor
 
-    # @param other [Vector] other operand.
-    # @return [Vector] eq of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:xor, '^')
-    define_binary_element_wise_operator(:equal, '==')
+    # Compare values for equality (self == other)
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method equal(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   eq of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :equal
+    alias_method :'==', :equal
     alias_method :eq, :equal
 
-    # @param other [Vector] other operand.
-    # @return [Vector] gt of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:greater, '>')
+    # Compare values for ordered inequality (self > other).
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method greater(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   gt of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :greater
+    alias_method :'>', :greater
     alias_method :gt, :greater
 
-    # @param other [Vector] other operand.
-    # @return [Vector] ge of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:greater_equal, '>=')
+    # Compare values for ordered inequality (self >= other).
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method greater_equal(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   ge of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :greater_equal
+    alias_method :'>=', :greater_equal
     alias_method :ge, :greater_equal
 
-    # @param other [Vector] other operand.
-    # @return [Vector] lt of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:less, '<')
+    # Compare values for ordered inequality (self < other).
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method less(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   lt of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :less
+    alias_method :'<', :less
     alias_method :lt, :less
 
-    # @param other [Vector] other operand.
-    # @return [Vector] le of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:less_equal, '<=')
+    # Compare values for ordered inequality (self <= other).
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method less_equal(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   le of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :less_equal
+    alias_method :'<=', :less_equal
     alias_method :le, :less_equal
 
-    # @param other [Vector] other operand.
-    # @return [Vector] ne of self and other by a boolean Vector.
-    define_binary_element_wise_operator(:not_equal, '!=')
+    # Compare values for inequality (self != other).
+    #
+    # A nil on either side emits a nil comparison result.
+    # @!method not_equal(other)
+    # @param other [Vector]
+    #   other vector or scalar.
+    # @return [Vector]
+    #   ne of self and other by a boolean Vector.
+    #
+    define_binary_element_wise :not_equal
+    alias_method :'!=', :not_equal
     alias_method :ne, :not_equal
+
+    # rubocop:enable Lint/SymbolConversion)
   end
 end
